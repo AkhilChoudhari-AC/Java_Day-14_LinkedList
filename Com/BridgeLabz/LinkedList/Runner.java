60f62c11145aaaa41b35fbabe85862536ab38f57
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-07-14 21:48:20 +0530

    REFACTOR: Ability to delete the last element

--- Com/BridgeLabz/LinkedList/Runner.java
+++ Com/BridgeLabz/LinkedList/Runner.java
@@ -8,14 +8,11 @@ public class Runner {
         LinkedList list = new LinkedList();
 
         list.insert(56);
+        list.insert(30);
         list.insert(70);
-        list.insertMid(2, 30);
-
         list.print();
-        System.out.println("First element is deleted");
-        int pop = list.popAtFirst();
-        System.out.println(pop);
-        System.out.println("After Deleting first element");
+
+        int pop = list.popAtLast();
         list.print();
     }
 }
