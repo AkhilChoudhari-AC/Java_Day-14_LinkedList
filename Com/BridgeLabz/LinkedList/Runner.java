3502b62bc61dcc9756cc36259e8aafe5dea6e9fe
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-07-14 23:01:37 +0530

    ADDED: Ability to delete the element from the list sequence

--- Com/BridgeLabz/LinkedList/Runner.java
+++ Com/BridgeLabz/LinkedList/Runner.java
@@ -13,7 +13,13 @@ public class Runner {
         list.print();
 
         list.searchNode(30);
+        /*
+         * Ability to insert 40 after 30 to the Linked List
+         */
         list.insertMid(3, 40);
+        list.print();
+        list.delete(40);
+        list.printSize();
         list.print();
 
     }
