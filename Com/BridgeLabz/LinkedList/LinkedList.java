1e8826593a59887de9101b386a8190c9e84dd029
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-07-14 21:24:51 +0530

    ADDED: Ability to adding in linkedlist

--- Com/BridgeLabz/LinkedList/LinkedList.java
+++ Com/BridgeLabz/LinkedList/LinkedList.java
@@ -2,39 +2,39 @@ package Com.BridgeLabz.LinkedList;
 
 public class LinkedList {
 
-    public static void main(String[] args) {
-
-        System.out.println("Welcome to LinkedList program");
-        LinkedList list = new LinkedList();
-        list.insert(56);
-        list.insert(30);
-        list.insert(70);
-        list.print();
-    }
-
-
     public MyNode head = null;
     public MyNode tail = null;
 
-
+    /*
+     * creating insert method
+     */
     public void insert(int data) {
-
-
+        /*
+         * Creating object as newNode
+         */
         MyNode newNode = new MyNode(data);
         if (head == null) {
-
+            /*
+             * If list is empty, both head and tail will point to new node
+             */
             head = newNode;
             tail = newNode;
         } else {
-
+            /*
+             * newNode will be added after tail such that tail's next will point to newNode
+             */
             tail.next = newNode;
             tail = newNode;
         }
     }
 
-
+    /*
+     * creating print method
+     */
     public void print() {
-
+        /*
+         * Node current will point to head
+         */
         MyNode temp = head;
         if (head == null) {
             System.out.println("List is empty");
@@ -42,10 +42,25 @@ public class LinkedList {
         }
         System.out.println("Nodes of linked list is: ");
         while (temp != null) {
-
+            /*
+             * Prints each node by incrementing pointer
+             */
             System.out.print(temp.data + " ");
             temp = temp.next;
         }
         System.out.println();
     }
+
+    /*
+     * addNode() method here new data is added inFront of data
+     */
+    public void addNode(int data) {
+        MyNode newNode = new MyNode(data);
+        newNode.next = head;
+        /*
+         * newNode will be added before head such that head next will point to newNode
+         */
+        head = newNode;
+    }
+
 }
---
+++ Com/BridgeLabz/LinkedList/Runner.java
@@ -0,0 +1,15 @@
+package Com.BridgeLabz.LinkedList;
+
+public class Runner {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to LinkedList program");
+
+        LinkedList list = new LinkedList();
+
+        list.addNode(70);
+        list.addNode(30);
+        list.addNode(56);
+        list.print();
+    }
+}
